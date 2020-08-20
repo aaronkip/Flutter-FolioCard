e00f57e517bd840c3da4ff7b505dff186ded2da1
Akron254 <aaronrono42@gmail.com>
2020-08-20 11:17:34 +0300

    Added Divider

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,13 @@ class MyApp extends StatelessWidget {
                   color: Colors.teal[100],
                 ),
               ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.teal[100],
+                ),
+              ),
               Card(
                 margin: EdgeInsets.symmetric(vertical:10.0, horizontal:25),
                 child: Padding(
